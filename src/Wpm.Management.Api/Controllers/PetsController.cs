d9870ee4b09e2ba21746c71edfb34ff5926b58b2
faizanalim <faizan.23@hotmail.com>
2025-01-20 06:00:21 +0500

    Add PetsController and Write Get Methods

---
+++ src/Wpm.Management.Api/Controllers/PetsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wpm.Management.Api.DataAccess;
+
+namespace Wpm.Management.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PetsController(ManagementDbContext dbContext) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var all = await dbContext.Pets.Include(p => p.Breed).ToListAsync();
+            return Ok(all);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var pet = await dbContext.Pets.Include(p => p.Breed)
+                .Where(p => p.Id == id)
+                .FirstOrDefaultAsync();
+            return Ok(pet);
+        }
+    }
+}
