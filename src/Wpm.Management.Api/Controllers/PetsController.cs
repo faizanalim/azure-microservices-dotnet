a888621eef177967601fb46b682db864dee867a5
faizanalim <faizan.23@hotmail.com>
2025-01-22 04:03:29 +0500

    Add validations

--- src/Wpm.Management.Api/Controllers/PetsController.cs
+++ src/Wpm.Management.Api/Controllers/PetsController.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Net;
 using Wpm.Management.Api.DataAccess;
 
 namespace Wpm.Management.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class PetsController(ManagementDbContext dbContext) : ControllerBase
+    public class PetsController(ManagementDbContext dbContext, ILogger<PetsController> logger) : ControllerBase
     {
         [HttpGet]
         public async Task<IActionResult> Get()
@@ -14,13 +16,39 @@ namespace Wpm.Management.Api.Controllers
             var all = await dbContext.Pets.Include(p => p.Breed).ToListAsync();
             return Ok(all);
         }
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = nameof(GetById))]
         public async Task<IActionResult> GetById(int id)
         {
             var pet = await dbContext.Pets.Include(p => p.Breed)
                 .Where(p => p.Id == id)
                 .FirstOrDefaultAsync();
             return Ok(pet);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewPet newPet)
+        {
+            try
+            {
+                var pet = newPet.ToPet();
+                await dbContext.Pets.AddAsync(pet);
+                await dbContext.SaveChangesAsync();
+
+                return CreatedAtRoute(nameof(GetById), new { id = pet.Id }, newPet);
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+        public record NewPet(string Name, int Age, int BreedId)
+        {
+            public Pet ToPet()
+            {
+                return new Pet() { Name = Name, Age = Age, BreedId = BreedId };
+            }
         }
     }
 }
